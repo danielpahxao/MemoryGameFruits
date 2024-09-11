1e9476fa40a0c841c0dc3981ba55661a38c4a127
Daniel Paixao <ddanielpaixaodasilva@gmail.com>
2024-09-11 08:26:22 -0300

    Preparing the first page

---
+++ MemoryGame/mg_script.js
@@ -0,0 +1,8 @@
+const mode6 = document.getElementsByClassName("mode")[0];
+
+const headerEl = document.getElementById("menu");
+
+function startGame() {
+    document.getElementById("menu").style.visibility = "hidden";
+    document.getElementById("menu").style.opacity = "0";
+};
